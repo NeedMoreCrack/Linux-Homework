ac632f7232419b6e02fb9b24d5b1087b469c661d
NeedMoreCrack <qrt0zblng@gmail.com>
2024-09-30 11:53:44 +0800

    修改增加使用者腳本

--- create_user1-3.sh
+++ create_user1-3.sh
@@ -1,11 +1,11 @@
-sudo useradd -m -s /bin/bash user1
-sudo useradd -m -s /bin/bash user2
-sudo useradd -m -s /bin/bash user3
+useradd -m -s /bin/bash user1
+useradd -m -s /bin/bash user2
+useradd -m -s /bin/bash user3
 echo "user1:user1" | sudo chpasswd
 echo "user2:user2" | sudo chpasswd
 echo "user3:user3" | sudo chpasswd
-sudo groupadd ios113
-sudo usermod -aG ios113 user1
-sudo usermod -aG ios113 user2
-sudo usermod -aG ios113 user3
+groupadd ios113
+usermod -aG ios113 user1
+usermod -aG ios113 user2
+usermod -aG ios113 user3
 
